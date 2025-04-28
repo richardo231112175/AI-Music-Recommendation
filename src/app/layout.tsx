604ade28c7a6a12ca3e8def0a2707396fc6d05b5
Richardo Lieberio <richardo.lieberio@students.mikroskil.ac.id>
2025-04-28 13:38:49 +0700

    Modified root layout and root page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css';
 export default function RootLayout({ children }: { children: ReactNode }): JSX.Element {
     return (
         <html lang="en">
-            <body className="antialiased">
+            <body className="min-w-80 text-sm md:text-base antialiased">
                 { children }
             </body>
         </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 import { JSX } from 'react';
+import Input from '@/components/Input';
 
 export default function RootPage(): JSX.Element {
     return (
-        <h1>Hello World</h1>
+        <>
+            <main className="max-w-3xl mx-auto px-4 md:px-8 relative"></main>
+            <div className="w-full max-w-3xl min-w-80 mx-auto px-4 md:px-8 fixed bottom-1/2 translate-y-1/2 left-1/2 -translate-x-1/2 space-y-4">
+                <h2 className="text-center text-xl md:text-2xl font-semibold">AI Music Recommendation</h2>
+                <Input />
+            </div>
+        </>
     );
 }
