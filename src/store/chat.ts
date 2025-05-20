757b9c4fc0c591c6bfc9622562b636e4eeac4807
Richardo Lieberio <richardo.lieberio@students.mikroskil.ac.id>
2025-05-20 10:20:15 +0700

    Added isPrompting to keep track if ai is prompting, modified messages type, and added more reducers

--- src/store/chat.ts
+++ src/store/chat.ts
@@ -1,29 +1,62 @@
 /* eslint-disable @typescript-eslint/typedef */
 
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import { v4 as uuidv4 } from 'uuid';
 
-export type messageType = {
-    sender: 'user' | 'ai',
-    text: string,
+type messageType = {
+    sender: 'user' | 'system';
+    text: string;
+} | {
+    sender: 'ai';
+    text: string[];
+};
+
+export type messageStateType = messageType & {
+    id: string;
 };
 
 type initialStateType = {
-    messages: messageType[];
-};
-
-const initialState: initialStateType = {
-    messages: [],
+    messages: messageStateType[];
+    isPrompting: boolean;
 };
 
 export const chatSlice = createSlice({
     name: 'chat',
-    initialState,
+    initialState: <initialStateType> {
+        messages: [],
+        isPrompting: false,
+    },
     reducers: {
         addChat: (state, action: PayloadAction<messageType>) => {
-            state.messages.push(action.payload);
+            const message: messageStateType = {
+                id: uuidv4(),
+                ...action.payload,
+            };
+            state.messages.push(message);
+        },
+        setPrompt: (state, action: PayloadAction<{ index: number, text: string }>) => {
+            const lastIndex: number = state.messages.length - 1;
+
+            if (lastIndex >= 0 && state.messages[lastIndex].sender === 'ai') {
+                state.messages[lastIndex].text[action.payload.index] = action.payload.text;
+            }
+        },
+        setSystemError: (state, action: PayloadAction<string>) => {
+            const lastIndex: number = state.messages.length - 1;
+
+            if (lastIndex >= 0 && state.messages[lastIndex].sender === 'ai') {
+                state.messages[lastIndex] = {
+                    id: state.messages[lastIndex].id,
+                    sender: 'system',
+                    text: action.payload,
+                };
+            }
+        },
+        setIsPrompting: (state, action: PayloadAction<boolean>) => {
+            state.isPrompting = action.payload;
         },
     },
 });
 
-export const { addChat } = chatSlice.actions;
+export const { addChat, setPrompt, setSystemError, setIsPrompting } = chatSlice.actions;
 export default chatSlice.reducer;
